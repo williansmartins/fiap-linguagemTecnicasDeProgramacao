59389ef7dd22208ebd5d82655f0b40be3f692868
unknown <pf0790@L1412MICRO01.fiap.com.br>
2015-04-10 11:12:59 -0300

    add conteudo da classe if

--- src/br/com/fiap/estruturaDeControle/selecao/If.java
+++ src/br/com/fiap/estruturaDeControle/selecao/If.java
@@ -1,5 +1,19 @@
 package br.com.fiap.estruturaDeControle.selecao;
 
 public class If {
-
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 20;
+		
+		if(a > b){
+			System.out.println("A > B");
+		}else{
+			if( a < b ){
+				System.out.println("B > A");
+			}else{
+				System.out.println("A == B");
+			}
+		}
+	}
 }
